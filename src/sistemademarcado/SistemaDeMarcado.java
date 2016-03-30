7280d93db5053c58f9dc9bbcd0939352626d7c31
Carlos Ortega <Carlos Ortega@Carlos>
2016-03-30 14:35:12 -0600

    Hice el Hola munda

--- src/sistemademarcado/SistemaDeMarcado.java
+++ src/sistemademarcado/SistemaDeMarcado.java
@@ -15,6 +15,7 @@ public class SistemaDeMarcado {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Hola mundo");
         // TODO code application logic here
     }
     
